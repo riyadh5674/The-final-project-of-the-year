5b240ae428883d2252ab2dfa919f8393975f9de2
Tanvir Tonoy || Modhu <happy2024.tat@gmail.com>
2025-12-08 06:06:58 +1100

    Mobile View - Links Toggle and  Animation - DONE

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,11 @@
-console.log("Alive!");
+function toggleMenu() {
+    const menuIcon = document.getElementById('menu-icon');
+    const navLinks = document.getElementById('nav-links');
+    menuIcon.addEventListener('click', () => {
+        console.log('Menu icon clicked');
+        navLinks.classList.toggle('active');
+    });
+}
+
+toggleMenu();
+
