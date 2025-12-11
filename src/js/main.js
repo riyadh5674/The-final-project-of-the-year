1f3eab729959554e27d12bd133fe2377acf525de
Tanvir Tonoy || Modhu <happy2024.tat@gmail.com>
2025-12-12 10:01:20 +1100

    Product Tiles - Initial styling and Prototype 01 Closed. New paged Added

--- src/js/main.js
+++ src/js/main.js
@@ -14,3 +14,13 @@ function toggleMenu(X,Y,Z = null){
 toggleMenu('menu-icon', 'nav-links');
 toggleMenu('toggle-left-menu', 'left-column','left-menu-content');
 
+
+
+// I want to add an click event in every prodcut tile to open a detail view of the product. when the user clicks in the tile, it will redirect to product-details.html with the product id as query parameter.
+const productTiles = document.querySelectorAll('.tile li');
+productTiles.forEach(tile => {
+    tile.addEventListener('click', () => {
+        const productId = tile.getAttribute('data-product-id');
+        window.location.href = `./src/pages/product-details.html?productId=${productId}`;
+    });
+});
